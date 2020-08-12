6692b341f255ad12a3f6c45bd54cf2fb018677cd
Denis Holdeew <h.denis@gmx.de>
2020-08-12 08:59:59 +0200

    Add examples

---
+++ src/main/java/com/holidaydrills/Main.java
@@ -0,0 +1,11 @@
+package com.holidaydrills;
+
+public class Main {
+
+    public static void main(String ...args) {
+        StringApiEnhancements.isBlankExample();
+        StringApiEnhancements.linesExample();
+       StringApiEnhancements.repeatExample();
+       StringApiEnhancements.stripExample();
+    }
+}
---
+++ src/main/java/com/holidaydrills/StringApiEnhancements.java
@@ -0,0 +1,47 @@
+package com.holidaydrills;
+
+import java.util.stream.Collectors;
+
+public class StringApiEnhancements {
+    public static void isBlankExample() {
+        String stringOfBlanks = "   ";
+        System.out.println(stringOfBlanks.isBlank()); // true
+
+        String notOnlyBlanks = "         not  only    blank   ";
+        System.out.println(notOnlyBlanks.isBlank()); // false
+    }
+
+    public static void linesExample() {
+        String multiLineString = "First line\nSecond line\nThird line";
+        System.out.println(multiLineString);
+        /*
+        Output:
+        First line
+        Second line
+        Third line
+         */
+
+        System.out.println(multiLineString.lines().collect(Collectors.toList()));
+        /*
+        Output:
+        [First line, Second line, Third line]
+         */
+    }
+
+    public static void repeatExample() {
+        String stringThatShouldBeRepeated = "Jump!";
+        System.out.println(stringThatShouldBeRepeated.repeat(5)); // Output: Jump!Jump!Jump!Jump!Jump!
+    }
+
+    public static void stripExample() {
+        String myString = "   Hello there!    ";
+        System.out.println(myString.strip()); // Output: "Hello there!"
+        System.out.println(myString.stripLeading()); // Output: "Hello there!    "
+        System.out.println(myString.stripTrailing()); // Output: "   Hello there!"
+
+        String withUnicodeWhitespace = "\u2005\u2005Hello there!  \n\t";
+        System.out.println(withUnicodeWhitespace.strip()); // Output: "Hello there!"
+        System.out.println(withUnicodeWhitespace.trim()); // Output: "  Hello there!"
+    }
+
+}
